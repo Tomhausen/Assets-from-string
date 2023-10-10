e14a1337ccfe7f2bd5dc411f5b36d287fa5fd0ba
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-10-10 16:38:12 +0100

    tested fully

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let asset = "red"
+let pic = images.getImageFromString(asset)
+let me = sprites.create(pic)
+let tile = images.getTileFromString(asset)
+asset = "level"
+let tm = images.getTilemapFromString(asset)
+scene.setTileMapLevel(tm)
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    asset = "duck"
+    let anim = images.getAnimationFromString(asset)
+    animation.runImageAnimation(me, anim, 100, true)
+})
+
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    tiles.setTileAt(me.tilemapLocation(), tile)
+})
